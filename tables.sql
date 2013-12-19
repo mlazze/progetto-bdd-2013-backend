ea67988a804400c0f32497bac05c85201769612f
staminskij <docxp0@gmail.com>
2013-12-19 11:55:20 +0100

    Added examples (ty generatedata.com), continued fixing err

--- tables.sql
+++ tables.sql
@@ -1,13 +1,22 @@
-set search_path='progetto';
+CREATE TABLE nazione(
+	id INTEGER ,
+	iso CHAR(2) NOT NULL,
+	name VARCHAR(80) NOT NULL,
+	nicename VARCHAR(80) PRIMARY KEY,
+	iso3 CHAR(3) DEFAULT NULL,
+	numcode INTEGER DEFAULT NULL,
+	phonecode INTEGER NOT NULL
+);
+
 CREATE TABLE utente(
 	userid SERIAL PRIMARY KEY , 
-	nome VARCHAR(20), 
+	nome VARCHAR(30), 
 	cognome VARCHAR(20), 
 	cfiscale CHAR(16) NOT NULL CHECK (cfiscale ~ '[A-Za-z0-9]{16}'), 
-	indirizzo VARCHAR(50), 
-	nazione_res VARCHAR(20), 
-	email VARCHAR(50), 
-	telefono VARCHAR(15) 
+	indirizzo VARCHAR(70), 
+	nazione_res VARCHAR(50) REFERENCES nazione(nicename),
+	email VARCHAR(100) CHECK (email ~ '^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$'), 
+	telefono VARCHAR(15) CHECK (telefono ~ '[+]?[0-9]*[/-\\]?[0-9]*')
 	);
 
 CREATE TABLE profilo(
@@ -31,7 +40,7 @@ CREATE TABLE conto(
 	amm_tettomax DECIMAL(12,2) NOT NULL,
 	tipo DEPCRED NOT NULL,
 	scadenza_giorni INTEGER,
-	giorno_iniziale INTEGER,
+	giorno_iniziale INTEGER CHECK (giorno_iniziale >= 1 AND giorno_iniziale <=31),
 	userid INTEGER REFERENCES utente(userid),
 	data_creazione DATE
 	);
@@ -54,8 +63,7 @@ CREATE TABLE entrate(
 	data DATE,
 	descrizione VARCHAR(100),
 	valore DECIMAL(12,2),
-	PRIMARY KEY(conto,id_op),
-	FOREIGN KEY(categoria_user,categoria_nome) REFERENCES categoria(userid,nome)
+	PRIMARY KEY(conto,id_op)
 	);
 
 CREATE TABLE bilancio(
@@ -78,3 +86,4 @@ CREATE TABLE associazione_bilancio(
 	FOREIGN KEY (userid,nome_cat) REFERENCES categoria(userid,nome),
 	FOREIGN KEY (userid,bilancio) REFERENCES bilancio(userid,nome)
 	);
+
