790fc0b71fbcab2e750778aeef8b547fb253353f
staminskij <docxp0@gmail.com>
2014-01-02 20:49:43 +0100

    Added fixall_til bilancio part

--- postfunct.sql
+++ postfunct.sql
@@ -5,6 +5,8 @@ CREATE OR REPLACE FUNCTION fixall_til(DATE) RETURNS VOID AS $$
 		b DECIMAL(19,4);
 		spesa_var spesa%ROWTYPE;
 		entrata_var entrata%ROWTYPE;
+		bil_var bilancio%ROWTYPE;
+		cat_var categoria_spesa%ROWTYPE;
 	BEGIN
 		--check conti credito, crea relative spese/entrate e aggiorna amm_disp
 		FOR conto_var IN (SELECT * FROM conto WHERE data_creazione <= $1 AND tipo='Credito') LOOP
@@ -92,6 +94,7 @@ CREATE OR REPLACE FUNCTION fixall_til(DATE) RETURNS VOID AS $$
 			END IF;
 		END LOOP;
 
+		--calcola disp conti di deposito
 		FOR conto_var IN (SELECT * from conto WHERE tipo='Deposito' AND data_creazione <= $1) LOOP
 			SELECT SUM(valore) INTO a FROM spesa WHERE conto = conto_var.numero AND data <= $1;
 			SELECT SUM(valore) INTO b FROM entrata WHERE conto = conto_var.numero AND data <= $1;
@@ -105,6 +108,36 @@ CREATE OR REPLACE FUNCTION fixall_til(DATE) RETURNS VOID AS $$
 				UPDATE conto SET amm_disp = a WHERE numero = conto_var.numero;
 			END IF;
 
+		END LOOP;
+
+		--calcola bilanci
+		FOR bil_var IN (SELECT * from bilancio) LOOP
+			--FOR conto_var IN (SELECT * from conto WHERE numero IN (SELECT numero_conto FROM bilancio_conto WHERE userid = bil_var.userid AND nome_bil = bil_var.nome_bil)) LOOP
+			--	FOR cat_var IN (SELECT * FROM categoria_spesa WHERE nome IN (SELECT nome_cat FROM bilancio_categoria WHERE userid = cat_var.userid AND nome_cat = cat_var.nome)) LOOP
+			WHILE (bil_var.data_partenza + bil_var.periodovalidita <= $1) LOOP
+				bil_var.data_partenza = bil_var.data_partenza + bil_var.periodovalidita;
+			END LOOP;
+
+			SELECT SUM(valore) INTO a FROM spesa WHERE conto IN (SELECT numero_conto FROM bilancio_conto WHERE userid = bil_var.userid AND nome_bil = bil_var.nome) AND categoria_nome IN /*(SELECT nome_cat FROM bilancio_categoria WHERE userid = bil_var.userid AND nome_bil = bil_var.nome)*/ (
+							WITH RECURSIVE rec_cat AS (
+								SELECT nome,userid,supercat_nome FROM categoria_spesa WHERE userid=1 AND nome IN (SELECT nome_cat FROM bilancio_categoria WHERE userid = 1 AND nome_bil = 'Agamennone')
+
+								UNION ALL
+
+								SELECT c.nome,c.userid,c.supercat_nome FROM categoria_spesa AS c JOIN rec_cat AS rc ON c.supercat_nome = rc.nome AND c.userid = rc.userid WHERE c.userid = 1 
+							)
+							SELECT nome FROM rec_cat
+						) AND data >= bil_var.data_partenza AND data <= $1;
+							
+			--RAISE NOTICE 'Spese per bilancio % utente % = %', bil_var.nome, bil_var.userid, a;
+			--RAISE NOTICE 'bil_var.userid = %, bil_var.nome = %, bil_var.data_partenza = %, D1 = %', bil_var.userid , bil_var.nome , bil_var.data_partenza , $1;
+			IF a IS NULL THEN
+				UPDATE bilancio SET ammontarerestante = ammontareprevisto WHERE userid = bil_var.userid AND nome = bil_var.nome;
+			--	RAISE NOTICE 'Bilancio % utente % settato a %', bil_var.nome, bil_var.userid, 10000;
+			ELSE
+				UPDATE bilancio SET ammontarerestante = ammontareprevisto - a WHERE userid = bil_var.userid AND nome = bil_var.nome;
+			--	RAISE NOTICE 'Bilancio % utente % settato a %', bil_var.nome, bil_var.userid, 10000-a;
+			END IF;
 		END LOOP;
 
 	END;
